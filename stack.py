c6ad361bff73c85cf7f24d1e422ef7ed5bb6ed0e
i5elousov <i5elousov@yandex.ru>
2024-04-20 18:06:56 +0300

    feat(project): realization push

--- stack.py
+++ stack.py
@@ -5,9 +5,23 @@ class ZootopiaTasksStack:
             self.__data = data
             self.__prev = None
 
-    def push(self, ZootopiaTask):
-        pass
+    def __init__(self):
+        self.__top = None
+        self.__size = 0
 
+    def push(self, ZootopiaTask):
+        """
+        Добавляет новую задачу в колекцию
+        :param ZootopiaTask: Объект класса ZootopiaTask
+        :return: None
+        """
+
+        new_node = ZootopiaTasksStack.Node(ZootopiaTask)
+        new_node.__prev = self.__top
+        self.__top = new_node
+
+        self.__size += 1
+              
     def pop(self):
         pass
 
