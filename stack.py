930c6b2ad9b8afc3078bc3177531812aa422072d
i5elousov <i5elousov@yandex.ru>
2024-04-20 18:10:59 +0300

    feat(project): realization pop

--- stack.py
+++ stack.py
@@ -21,9 +21,21 @@ class ZootopiaTasksStack:
         self.__top = new_node
 
         self.__size += 1
-              
+
     def pop(self):
-        pass
+        """
+        Удаляет последнюю задачу из коллекции и возвращает нам ее
+        :return: Удаленная задача
+        """
+        
+        if self.__size == 0:
+            return None
+
+        data = self.__top.__data
+        self.__top = self.__top.__perv
+        self.__size -= 1
+
+        return data
 
     def peek(self):
         pass
