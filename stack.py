f8e52aa8cbfa462e6807dbcc1234c7604e384eff
i5elousov <i5elousov@yandex.ru>
2024-04-20 19:59:31 +0300

    style(porject:stack) add anotation

--- stack.py
+++ stack.py
@@ -1,7 +1,7 @@
 class ZootopiaTasksStack:
 
     class Node:
-        def __init__(self, data):
+        def __init__(self, data: any):
             self.data = data
             self.prev = None
 
@@ -9,20 +9,20 @@ class ZootopiaTasksStack:
         self.__top = None
         self.__size = 0
 
-    def push(self, ZootopiaTask):
+    def push(self, zootopia_task: any) -> None:
         """
         Добавляет новую задачу в колекцию
         :param ZootopiaTask: Объект класса ZootopiaTask
         :return: None
         """
 
-        new_node = ZootopiaTasksStack.Node(ZootopiaTask)
+        new_node = ZootopiaTasksStack.Node(zootopia_task)
         new_node.prev = self.__top
         self.__top = new_node
 
         self.__size += 1
 
-    def pop(self):
+    def pop(self) -> None or any:
         """
         Удаляет последнюю задачу из коллекции и возвращает нам ее
         :return: Удаленная задача
@@ -37,14 +37,14 @@ class ZootopiaTasksStack:
 
         return data
 
-    def peek(self):
+    def peek(self) -> any:
         """
         Возвращает последнюю добаленную задачу из коллекции
         :return: Последняя внесенная задача в коллекцию
         """
         return self.__top.data if self.__size != 0 else None
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """
         Проверка коллекции на пустоту
         :return: True - если коллекция пуста
@@ -52,7 +52,7 @@ class ZootopiaTasksStack:
         """
         return self.__top is not None
 
-    def count(self):
+    def count(self) -> int:
         """
         Возвращает количство задач в коллекции
         :return: возвращает натуральное число
