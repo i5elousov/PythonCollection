cc94dac8b45e159afec0dd8ff9167a3df1128fa9
i5elousov <i5elousov@yandex.ru>
2024-04-20 19:32:25 +0300

    feat(project:zootopia_simulation): add magic method str

--- zootopia_simulation.py
+++ zootopia_simulation.py
@@ -1,6 +1,10 @@
 from datetime import datetime
+import stack
 class ZootopiaTask:
     def __init__(self, discription: str, duedata=None):
         self.__discription = discription
         self.__duedata = duedata
 
+    def __str__(self):
+        return (f'Описание задачи: {self.__discription}\n'
+                f'Срок выполнения: до {self.__duedata}')
